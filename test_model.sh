090689cdcc4d48a05ab7dd85e221451f82b84904
ramon <rlcorre@emory.edu>
2020-03-28 18:06:01 -0400

    more stuff

--- test_model.sh
+++ test_model.sh
@@ -1,10 +1,12 @@
 echo " Starting test" 
 for i in {1..5};
 do 
-mkdir "output${i}"
-java -jar RankLib.jar -norm zscore -load "./base_model_f${i}.txt" -test "./MQ2007/Fold${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/baseline.txt"
-java -jar RankLib.jar -norm zscore -load "./aug_model_f${i}.txt" -test "./augData/Fold_${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/aug.txt"
-java -cp RankLib.jar ciir.umass.edu.eval.Analyzer -all "./output${i}/" -base baseline.txt > "analysis${i}.txt"
-done 
+mkdir -p "output${i}/score"
+mkdir -p "output${i}/comp"
+java -jar RankLib.jar -norm zscore -load "./base_model_f${i}.txt" -test "./MQ2008/Fold${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/comp/base.txt"
+java -jar RankLib.jar -load "./base_model_f${i}.txt" -rank "./MQ2008/Fold${i}/test.txt" -score "./output${i}/score/base_ScoreFile.txt"
+java -jar RankLib.jar -norm zscore -load "./aug_model_f${i}.txt" -test "./augData1/Fold_${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/comp/aug.txt"
+java -jar RankLib.jar -load "./aug_model_f${i}.txt" -rank "./augData1/Fold_${i}/test.txt" -score "./output${i}/score/aug_ScoreFile.txt"
 
-java -cp RankLib.jar ciir.umass.edu.eval.Analyzer -all "./output1/" -base baseline.txt > "analysis_general.txt"
+java -cp RankLib.jar ciir.umass.edu.eval.Analyzer -all "./output${i}/comp" -base base.txt > "analysis${i}.txt"
+done 
