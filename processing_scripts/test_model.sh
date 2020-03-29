e8237b0c7598333566d75432d022439fe9c51759
ramon <rlcorre@emory.edu>
2020-03-29 19:33:34 -0400

    please work

--- processing_scripts/test_model.sh
+++ processing_scripts/test_model.sh
@@ -1,11 +1,14 @@
 echo " Starting test" 
-for i in {1..5};
-do 
-mkdir -p "output${i}/score"
-mkdir -p "output${i}/comp"
-java -jar RankLib.jar -norm zscore -load "./base_model_f${i}.txt" -test "./MQ2008/Fold${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/comp/base.txt"
-java -jar RankLib.jar -load "./base_model_f${i}.txt" -rank "./MQ2008/Fold${i}/test.txt" -score "./output${i}/score/base_ScoreFile.txt"
-java -jar RankLib.jar -norm zscore -load "./aug_model_f${i}.txt" -test "./augData1/Fold_${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/comp/aug.txt"
-java -jar RankLib.jar -load "./aug_model_f${i}.txt" -rank "./augData1/Fold_${i}/test.txt" -score "./output${i}/score/aug_ScoreFile.txt"
-java -cp RankLib.jar ciir.umass.edu.eval.Analyzer -all "./output${i}/comp" -base base.txt > "analysis${i}.txt"
+for year in {2007,2008};
+    models_path="./${year}_models/"
+    for i in {1..5};
+    do
+        mkdir -p "./${year}_models/output${i}/score"
+        mkdir -p "./${year_models}output${i}/comp"
+        java -jar RankLib.jar -norm zscore -load "./base_model_f${i}.txt" -test "./MQ2008/Fold${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/comp/base.txt"
+        java -jar RankLib.jar -load "./base_model_f${i}.txt" -rank "./MQ2008/Fold${i}/test.txt" -score "./output${i}/score/base_ScoreFile.txt"
+        java -jar RankLib.jar -norm zscore -load "./aug_model_f${i}.txt" -test "./augData1/Fold_${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/comp/aug.txt"
+        java -jar RankLib.jar -load "./aug_model_f${i}.txt" -rank "./augData1/Fold_${i}/test.txt" -score "./output${i}/score/aug_ScoreFile.txt"
+        java -cp RankLib.jar ciir.umass.edu.eval.Analyzer -all "./output${i}/comp" -base base.txt > "analysis${i}.txt"
+    done 
 done 
--- processing_scripts/train_model.sh
+++ processing_scripts/train_model.sh
@@ -2,13 +2,16 @@
 for year in {2007,2008}; 
 do 
     echo "training models for ${year}"
+    models_path="./${year}_models"
+    mkdir "${models_path}" 
     for i in {1..5}; 
     do 
+        base_path="./MQ${year}/Fold${i}"
+        aug_path="./${year}_augData/Fold_${i}"
         echo "Starting model training fold ${i}"
-        java -jar RankLib.jar -norm zscore -train "./MQ${year}/Fold${i}/train.txt" -test "./MQ${year}/Fold${i}/test.txt" -validate "./MQ${year}/Fold${i}/vali.txt" -ranker 6 -metric2t NDCG@10 -metric2T NDCG@10 -save "${year}_base_model_f${i}.txt"
+        java -jar RankLib.jar -norm zscore -train "${base_path}/train.txt" -test ".${base_path}/test.txt" -validate "${base_path}/vali.txt" -ranker 6 -metric2t NDCG@10 -metric2T NDCG@10 -save "${models_path}/${year}_base_model_f${i}.txt"
         echo " Start training augmented model "
-        java -jar RankLib.jar -norm zscore -train "./${year}_augData/Fold_${i}/train.txt" -test "./${year}_augData/Fold_${i}/test.txt" -validate "./${year}_augData/Fold_${i}/vali.txt" -ranker 6 -metric2t NDCG@10 -metric2T NDCG@10 -save "${year}_aug_model_f${i}.txt"
-        break 
+        java -jar RankLib.jar -norm zscore -train "${aug_path}/train.txt" -test "${aug_path}/test.txt" -validate "./${aug_path}/vali.txt" -ranker 6 -metric2t NDCG@10 -metric2T NDCG@10 -save "${models_path}/${year}_aug_model_f${i}.txt"
     done 
     break 
 done 
