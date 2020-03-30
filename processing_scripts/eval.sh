4bc08445e39ca7180f7c8a8685536ec1aeaa4f07
ramon <rlcorre@emory.edu>
2020-03-29 21:08:43 -0400

    updated scripts to do comparison with perl scrip

--- processing_scripts/eval.sh
+++ processing_scripts/eval.sh
@@ -1,8 +1,16 @@
-for i in {1..5};
+for year in {2007,2008};
 do 
-    cd "./output${i}/" 
-    perl ../mslr-eval-score-mslr.pl "../MQ2008/Fold${i}/test.txt" ./new_base_ScoreFile.txt RAMEN_d.txt 0
-    echo " Doing other test "
-    perl ../mslr-eval-score-mslr.pl "../MQ2008/Fold${i}/test.txt" ./new_aug_ScoreFile.txt RAMEN_c.txt 0
-    cd ../ 
+    for i in {1..5};
+    do 
+        perl_score="./${year}_models/output${i}/perlScore"
+        analysis_path="./${year}_analysis"
+        perl ./processing_scripts/mslr-eval-score-mslr.pl "./MQ${year}/Fold${i}/test.txt" "${perl_score}/base_ScoreFile.txt" "${analysis_path}/perl_comp_${i}_base.txt" 0
+        perl ./processing_scripts/mslr-eval-score-mslr.pl "./MQ${year}/Fold${i}/test.txt" "${perl_score}/aug_ScoreFile.txt" "${analysis_path}/perl_comp_${i}_aug.txt" 0
+        echo "---- baseline---" > "${analysis_path}/merged_comp_${i}.txt"
+        cat "${analysis_path}/perl_comp_${i}_base.txt" >> "${analysis_path}/merged_comp_${i}.txt"
+        echo "----- aug ----" >> "${analysis_path}/merged_comp_${i}.txt"
+        cat "${analysis_path}/perl_comp_${i}_aug.txt" >> "${analysis_path}/merged_comp_${i}.txt"
+        rm "${analysis_path}/perl_comp_${i}_base.txt"
+        rm "${analysis_path}/perl_comp_${i}_aug.txt" 
+    done 
 done 
--- processing_scripts/process.sh
+++ processing_scripts/process.sh
@@ -1,10 +1,13 @@
 
-#processingFiles 
-
-for i in {1..5};
+#This script processes the score files so the perl script can process them 
+for year in {2007,2008}; 
 do 
-    cd "./output${i}/" 
-    awk '{print $3}' ./base_ScoreFile.txt >  new_base_ScoreFile.txt 
-    awk '{print $3}' ./aug_ScoreFile.txt >  new_aug_ScoreFile.txt 
-    cd ../ 
+    for i in {1..5};
+    do 
+        models_path="./${year}_models/output${i}/score"
+        perl_score="./${year}_models/output${i}/perlScore"
+        mkdir -p "${perl_score}" 
+        awk '{print $3}' "${models_path}/base_ScoreFile.txt" > "${perl_score}/base_ScoreFile.txt"
+        awk '{print $3}' "${models_path}/aug_ScoreFile.txt" >  "${perl_score}/aug_ScoreFile.txt" 
+    done 
 done 
