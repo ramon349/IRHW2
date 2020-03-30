9121421a406223f0ac25d8eb492fc3466744c06e
ramon <rlcorre@emory.edu>
2020-03-29 20:42:03 -0400

    test_script works now

--- processing_scripts/test_model.sh
+++ processing_scripts/test_model.sh
@@ -1,14 +1,20 @@
 echo " Starting test" 
 for year in {2007,2008};
-    models_path="./${year}_models/"
+do
+    models_path="./${year}_models"
+    analysis_path="./${year}_analysis"
+    mkdir -p "${analysis_path}"
     for i in {1..5};
     do
-        mkdir -p "./${year}_models/output${i}/score"
-        mkdir -p "./${year_models}output${i}/comp"
-        java -jar RankLib.jar -norm zscore -load "./base_model_f${i}.txt" -test "./MQ2008/Fold${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/comp/base.txt"
-        java -jar RankLib.jar -load "./base_model_f${i}.txt" -rank "./MQ2008/Fold${i}/test.txt" -score "./output${i}/score/base_ScoreFile.txt"
-        java -jar RankLib.jar -norm zscore -load "./aug_model_f${i}.txt" -test "./augData1/Fold_${i}/test.txt" -metric2T NDCG@10 -idv "./output${i}/comp/aug.txt"
-        java -jar RankLib.jar -load "./aug_model_f${i}.txt" -rank "./augData1/Fold_${i}/test.txt" -score "./output${i}/score/aug_ScoreFile.txt"
-        java -cp RankLib.jar ciir.umass.edu.eval.Analyzer -all "./output${i}/comp" -base base.txt > "analysis${i}.txt"
+        output_path="./${year}_models/output${i}"
+        base_data="./MQ${year}/Fold${i}"
+        aug_data="./${year}_augData/Fold_${i}"
+        mkdir -p "${output_path}/score"
+        mkdir -p "${output_path}/comp"
+        java -jar RankLib.jar -norm zscore -load "${models_path}/${year}_base_model_f${i}.txt" -test "${base_data}/test.txt" -metric2T NDCG@10 -idv "${output_path}/comp/base.txt"
+        java -jar RankLib.jar -load "${models_path}/${year}_base_model_f${i}.txt" -rank "${base_data}/test.txt" -score "${output_path}/score/base_ScoreFile.txt"
+        java -jar RankLib.jar -norm zscore -load "${models_path}/${year}_aug_model_f${i}.txt" -test "${aug_data}/test.txt" -metric2T NDCG@10 -idv "${output_path}/comp/aug.txt"
+        java -jar RankLib.jar -load "${models_path}/${year}_aug_model_f${i}.txt" -rank "${aug_data}/test.txt" -score "${output_path}/score/aug_ScoreFile.txt"
+        java -cp RankLib.jar ciir.umass.edu.eval.Analyzer -all "${output_path}/comp" -base base.txt > "${analysis_path}/analysis_fold${i}.txt"
     done 
 done 
